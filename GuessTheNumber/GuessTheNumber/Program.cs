12b29dc4f614c847f05168cf8d1e0c92f31ed73e
Lizelmurigi <47571321+Lizelmurigi@users.noreply.github.com>
2021-06-09 09:22:48 +0300

    Add files via upload

---
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomizer = new Random();
+            int divisible = randomizer.Next(1, 75);
+            int oddNumber = randomizer.Next(1, 75);
+            int guess = 0;
+
+            while (guess != divisible)
+            {
+                // Let the player enter a number
+                Console.WriteLine("What is your guess?");
+                string guessText = Console.ReadLine();
+                // Evaluate the player's guess and respond
+                guess = int.Parse(guessText);
+                if (guess == divisible)
+                {
+                    Console.WriteLine("You Win! 75 points");
+
+                }
+
+
+                else if (guess == oddNumber)
+
+                { Console.WriteLine("Lose! 0 Points"); }
+            }
+
+            
+        }
+    }
+}
